8d45db520d1ceca4d8855099864cb59becae5ab7
NhanDev <huynhthanhnhan632004@gmail.com>
2024-12-17 13:38:49 +0700

    commitMessage

--- backend/src/apis/models/products/products.service.ts
+++ backend/src/apis/models/products/products.service.ts
@@ -7,6 +7,7 @@ import { Request } from 'express';
 import { ProductCategoriesService } from '../product-categories/product-categories.service';
 import { AQueries } from 'src/abstracts/common/ABaseQueries.abstracts';
 import { UtilORM } from 'src/utils/orm.uutils';
+import { EAgeGroup } from 'src/enums/EAge.enum';
 
 @Injectable()
 export class ProductsService {
@@ -53,6 +54,11 @@ export class ProductsService {
             .select(fields)
             .skip({ limit, page })
             .take({ limit })
+            .where({
+                // "pc_category": "54ccfb6d-a2f5-404f-a59b-d8a780143a8d",
+                // "prod_agePlay": EAgeGroup.Under1Year,
+                "prod_sku": "sku 321"
+            })
 
         const queryBuilder: SelectQueryBuilder<ProductsEntity> = result.build();
 
--- backend/src/utils/orm.uutils.ts
+++ backend/src/utils/orm.uutils.ts
@@ -1,6 +1,7 @@
 import { Repository, SelectQueryBuilder } from "typeorm";
 import { calculatorSkipPage } from "./caculator.utils";
 import { CONST_VAL } from "src/constants/value.contants";
+import { start } from "repl";
 
 export class UtilORM<T> {
     private queryBuilder: SelectQueryBuilder<T>;
@@ -11,7 +12,46 @@ export class UtilORM<T> {
         this.queryBuilder = repo.createQueryBuilder(aliasName);
     }
 
-    select(fields: Array<T> = []) {
+    leftJoinAndSelect(fields: Array<keyof T>): this {
+        let arrFields: Array<string> = [];
+        for (let i = 0; i < fields.length; i++) {
+            arrFields.push(String(fields[i]));
+            this.queryBuilder.leftJoinAndSelect(
+                `${this.aliasName}.${String(fields[i])}`,
+                `${String(fields[i])}`)
+        }
+        return this
+    }
+
+    where(filter: Partial<Record<keyof T, string | number | [number, number]>>): this {
+
+        let isFirstQuery = true; // Đặt biến ở bên ngoài vòng lặp để kiểm tra lần đầu
+
+        for (let key in filter) {
+            const value = filter[key];
+            if (value !== undefined) {
+                // Kiểm tra nếu giá trị là mảng (ví dụ: cho khoảng giá trị)
+                if (Array.isArray(value)) {
+                    this.queryBuilder.andWhere(`${this.aliasName}.${key} BETWEEN :start AND :end`, {
+                        start: value[0],
+                        end: value[1]
+                    });
+                } else {
+                    if (isFirstQuery) {
+                        console.log(123);
+                        this.queryBuilder.where(`${this.aliasName}.${key} = :value`, { value });
+                        isFirstQuery = false;
+                    } else {
+                        this.queryBuilder.andWhere(`${this.aliasName}.${key} = :value`, { value });
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+
+    select(fields: Array<T> = []): this {
         let arrFields: Array<string> = [];
         arrFields.push(`${this.aliasName}.id`);
         if (Array.isArray(fields)) {
@@ -27,26 +67,14 @@ export class UtilORM<T> {
         return this
     }
 
-    leftJoinAndSelect(fields: Array<keyof T>) {
-        let arrFields: Array<string> = [];
-        for (let i = 0; i < fields.length; i++) {
-            arrFields.push(String(fields[i]));
-            this.queryBuilder.leftJoinAndSelect(
-                `${this.aliasName}.${String(fields[i])}`,
-                `${String(fields[i])}`)
-        }
-        console.log(arrFields);
-        return this
-    }
-
-    skip({ page, limit }: { page: number, limit: number }) {
+    skip({ page, limit }: { page: number, limit: number }): this {
         this.queryBuilder.skip(
             calculatorSkipPage({ limit, page })
         );
         return this;
     }
 
-    take({ limit }: { limit: number }) {
+    take({ limit }: { limit: number }): this {
         this.queryBuilder.take(limit || CONST_VAL.LIMIT_DEFAULT)
         return this;
     }
